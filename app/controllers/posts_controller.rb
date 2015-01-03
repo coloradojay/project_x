63c3615a2da63e8811227bf6a0bf6ccb13b3a7ef
coloradojay <delarosa.j@gmail.com>
2015-01-02 22:44:03 -0800

    Adding create, read, update functions and pages.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,9 +7,40 @@ class PostsController < ApplicationController
   	@post = Post.new
   end
 
+	def edit
+		# this action is responsible for retrieving a specific bean
+		@post = Post.find(params[:id])
+	end
+
   def create
+  	@post = Post.new(post_params)
+		# if the new post creation & save is sucessful, we want to redirect to the index
+		if @post.save
+			redirect_to posts_path
+		else
+			#if unsuccessful, reset to the new page
+			render "new"
+		end
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
+
+  def update # this action is responsible for saving an update to a specific post
+		# Retrieving a specific post from the database
+		@post = Post.find(params[:id])
+
+		if @post.update_attributes(params.require(:post).permit(:title, :body, :url))
+			redirect_to posts_path
+		else
+			#if unsuccessful, show to the edit page
+			render "edit"
+		end
+	end
+
+  private
+		def post_params
+		 	params.require(:post).permit(:title, :body, :url)
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+posts = Post.create([
+	{title: "Bulldog woes", body: "Please help find my lost Bulldog" },
+	{title: "Lost Bird", body: "Lost Bird, responds to Louie"}
+	])
